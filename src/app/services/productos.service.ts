02445ae951329e63571cdc36390116926644e355
Richard Marin <yeikost452@gmail.com>
2018-08-27 16:56:43 -0500

    Fin sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -2,23 +2,56 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {productoInterface} from '../../interface/producto.interface';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
   cargando = true;
   productos: productoInterface[] = [];
+  productosFIltrado: productoInterface[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
   }
 
-  private cargarProductos(){
-    this.http.get('https://angular-html-11928.firebaseio.com/productos_idx.json')
-      .subscribe( (resp: productoInterface[]) => {
-        this.productos = resp;
-        this.cargando = false;
-        console.log(resp);
+  private cargarProductos() {
+    return new Promise( (resolve, reject) => {
+      this.http.get('https://angular-html-11928.firebaseio.com/productos_idx.json')
+        .subscribe( (resp: productoInterface[]) => {
+          this.productos = resp;
+          this.cargando = false;
+          resolve();
+        });
+    });
+  }
+  getProducto(id: string) {
+    return this.http.get( `https://angular-html-11928.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto( termino: string ) {
+    if ( this.productos.length === 0 ) {
+      //cargar productos
+      this.cargarProductos().then( () => {
+        //ejecuta despz de tener los productos
+        //aplica el filtro
+        this.filtrarProductos( termino );
       });
+    } else {
+      //aplica el filtro
+      this.filtrarProductos( termino );
+    }
+  }
+  private filtrarProductos( termino: string) {
+    termino = termino.toLowerCase();
+    console.log(this.productos);
+    this.productosFIltrado = [];
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo.toLowerCase();
+      if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        this.productosFIltrado.push( prod );
+      }
+    });
   }
 }
